5ec2af0d265589bf980d793b43bec364fac71b81
arvid hallberg <you@example.com>
2022-12-11 21:45:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useLocalStorage from "use-local-storage";
+
+import "./index.css";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-theme={theme}>
+      <div className="login">
+        <h1> Login</h1>
+        <div className="container">
+          <div className="top">
+            <i className="fab fa-apple"></i>
+            <i className="fab fa-facebook-square"></i>
+            <i className="fab fa-twitter-square"></i>
+          </div>
+
+          <p className="divider">
+            <span>or</span>
+          </p>
+          <form>
+            <label>E-mail</label>
+
+            <input type="email" placeholder="Enter you email" />
+            <label>Password</label>
+            <input type="password" placeholder="Enter you password" />
+            <div className="remember">
+              <input type="checkbox" />
+              <p>Remember me</p>
+            </div>
+            <button> Log in</button>
+          </form>
+
+          <div className="bottom">
+            <p>Forgot your password?</p>
+            <a href="/"> Reset password</a>
+          </div>
+          <p className="create">Create Account</p>
+        </div>
+        <div className="theme-toggle">
+          {theme === "light" ? <h4>Light Theme</h4> : <h4>Dark Theme</h4>}
+          {theme === "light" ? (
+            <i onClick={switchTheme} class="fas fa-toggle-on"></i>
+          ) : (
+            <i onClick={switchTheme} class="fas fa-toggle-off"></i>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
